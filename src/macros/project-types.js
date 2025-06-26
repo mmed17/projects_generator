144a1d6e17cf5a7119cec72e19ce6627e5958e61
Mohamed EL BAKKALI <mmed172017@gmail.com>
2025-06-26 11:00:26 +0100

    fixing types

--- src/macros/project-types.js
+++ src/macros/project-types.js
@@ -4,7 +4,6 @@ import { APP_ID } from './app-id.js';
 export const PROJECT_TYPES = [
 	{ id: 0, label: t(APP_ID, 'Combi') },
 	{ id: 1, label: t(APP_ID, 'Solo Elektra ') },
-	{ id: 2, label: t(APP_ID, 'Research Project') },
-	{ id: 3, label: t(APP_ID, 'Solo Water') },
-	{ id: 4, label: t(APP_ID, 'Custom ') }
+	{ id: 2, label: t(APP_ID, 'Solo Water') },
+	{ id: 3, label: t(APP_ID, 'Custom ') }
 ];
